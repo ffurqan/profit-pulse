3ca6ff49f78b4ec935b179edddd031523036a64c
ffurqan <furqanlatif357@gmail.com>
2024-09-06 16:00:01 +0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -260,6 +260,18 @@ export default function Home() {
 export async function getServerSideProps(context) {
   const session = await getSession(context);
   console.log(session);
+  console.log(session);
+  console.log(session);
+  console.log(session);
+  console.log(session);
+  console.log(session);
+  console.log(session);
+  console.log(session);
+  console.log(session);
+  console.log(session);
+  console.log(session);
+  console.log(session);
+
 
   if (!session) {
     return {
