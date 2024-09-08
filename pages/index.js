8943889f09842e7f922755b8272418e85116eb69
ffurqan <furqanlatif357@gmail.com>
2024-09-08 18:55:19 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
         href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&amp;family=Nunito:wght@600;700;800&amp;display=swap"
         rel="stylesheet"
         crossOrigin="true"
-      />  
+      />
       <link
         href="cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css"
         rel="stylesheet"
@@ -486,17 +486,7 @@ export default function Home() {
         </div>
       </div>
 
-      <Footer />        
-<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
-<script>
-  new WOW().init();
-</script>
- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
-  <script src="main.js"></script>
+      <Footer />
       <Script src="/js/jquery.min.js" strategy="beforeInteractive" />
       <Script src="/js/bootstrap.min.js" strategy="lazyOnload" />
       <Script src="/lib/wow/wow.min.js" strategy="lazyOnload" />
